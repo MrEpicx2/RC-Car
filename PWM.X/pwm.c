00fc890ff20931cdc10775a34a65aab3303e64b9
MrEpicx2 <xavierb2@outlook.com>
2025-02-20 15:31:35 -0700

    PWM for servo and screen testing
    Servo test code finished up as well as some screen test code

--- PWM.X/pwm.c
+++ PWM.X/pwm.c
@@ -7,22 +7,54 @@
 
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
-    CCP = 0XD8;                 // unlock protected I/O registers
+    CCP = 0xD8;                 // unlock protected I/O registers - page 41
     CLKCTRL.OSCHFCTRLA = 0x14;  // Clock set to 8 MHz
     while (CLKCTRL.MCLKSTATUS & 0b00000001) {
         
     }
     // We want to generate a PWM waveform using TCA0 and pin PA0. the frequency will be 1KHz
     
-    TCA0.SINGLE.CTRLA = (0x01) << 3 | 0x01;     //Divide by 16 and enable perferal
+    TCA0.SINGLE.CTRLA = (TCA_SINGLE_CLKSEL_DIV256_gc) | (TCA_SINGLE_ENABLE_bm);     //Divide the 8Mhz by 256 and enable perferal
     TCA0.SINGLE.CTRLB = 0x03;                   // Single slope PWM
     TCA0.SINGLE.CTRLB |= 0x01 << 4;             // PA0 enable as single slope PWM
-    TCA0.SINGLE.PER = 500;                      // Frequency set to 1 KHz
-    TCA0.SINGLE.CMP0 = 250;                     // Set to 50% duty cycle
-    
+    TCA0.SINGLE.PER = 625;                      // Frequency set to 50 Hz page 236: (31250/625) 625, being the PER value
+    TCA0.SINGLE.CMP0 = 31;                      // Set to 5% duty cycle (31 is left, 47 middle, 63 is left)
     PORTA.DIRSET = 0x01;                        // PA0 set as output
+    
+    // Enable global interrupts.
+    SREG = 0b10000000;
+    
+    // Set the ADC reference level to VDD.
+    VREF.ADC0REF = 0b10000101;
+    
+    // Enable the ADC interrupt.
+    ADC0.INTCTRL = 0b00000001;
+    
+    // Select PD2 (AIN2) as the ADC input.
+    ADC0.MUXPOS = 0x02;
+
+    // Select minimum clock divide.
+    ADC0.CTRLC = 0x00;
+    
+    // Select single ended mode, 12 bit resolution and free-running modes.
+    ADC0.CTRLA = 0b00000011;
+    
+    // Start conversion.
+    ADC0.COMMAND = 0x01;
+    
+    uint16_t result;
+    uint16_t servo_position;
+    
     while (1) {
+        if (ADC0.INTFLAGS & 0b00000001) {
+            result = (ADC0.RESH << 8) | ADC0.RESL;
+            ADC0.INTFLAGS = 0b00000001;
+        }
+        servo_position = 31 + ((result * 32UL) / 4095);
+        TCA0.SINGLE.CMP0 = servo_position;
+        _delay_ms(10);
     }
 }
--- screen.X/newavr-main.c
+++ screen.X/newavr-main.c
@@ -13,6 +13,33 @@
 
 
 int main(void) {
+    
+    // Enable global interrupts.
+    SREG = 0b10000000;
+    
+    // Set the ADC reference level to VDD.
+    VREF.ADC0REF = 0b10000101;
+    
+    // Enable the ADC interrupt.
+    ADC0.INTCTRL = 0b00000001;
+    
+    // Select PD2 (AIN2) as the ADC input.
+    ADC0.MUXPOS = 0x02;
+
+    // Select minimum clock divide.
+    ADC0.CTRLC = 0x00;
+    
+    // Select single ended mode, 12 bit resolution and free-running modes.
+    ADC0.CTRLA = 0b00000011;
+    
+    // Start conversion.
+    ADC0.COMMAND = 0x01;
+    
+
+    int result_low = 0;
+    int result_high = 0;
+    
+    
     i2c_init(); // Initialize I2C
 
     _delay_ms(500); // Give I2C some time before checking
@@ -24,5 +51,29 @@ int main(void) {
     lcd_puts("Hello World1");  // Put string from RAM
     lcd_gotoxy(0, 2);         // Move cursor to line 3
     lcd_puts_p(PSTR("String from flash")); 
-    while(1); // Keep program running
+    _delay_ms(3000);
+    lcd_clrscr();
+    while(1) {
+        if (ADC0.INTFLAGS & 0b00000001) {
+            result_low = ADC0.RESL;
+            result_high = ADC0.RESH;
+            ADC0.INTFLAGS = 0b00000001;
+        }
+        
+        if (result_high == 0x04 && result_low == 0x00) {
+            lcd_clrscr();
+            lcd_puts("0 - 1.25v");
+        }
+        
+        else if (result_high == 0x07 && result_low == 0x33) {
+            lcd_clrscr();
+            lcd_puts("1.25v - 2.25v");
+        }
+        
+        else if (result_high == 0x0A && result_low == 0x66) {
+            lcd_clrscr();
+            lcd_puts("2.25v - 5v");
+        }
+    }
+    
 }
