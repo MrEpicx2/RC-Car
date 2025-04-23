ee024d1749240f98fb4b0f82bd429b8c03bece4d
Huynh Tran Khai <huynhkhai1711@gmail.com>
2025-04-22 20:57:57 -0600

    Metal detection works

--- RC-Car.X/avr-main.c
+++ RC-Car.X/avr-main.c
@@ -22,10 +22,15 @@
 bool fwd_on = false;
 bool bwd_on = false;
 
+bool right_light = false;
+bool left_light = false;
+
 uint16_t value[5];
 uint8_t count = 0;
 uint16_t distance[5];
 
+ActionFunction output = NULL;
+void* output_arg = NULL;
 
 void bwd(void* mode) {
     if ((uint8_t)(uintptr_t) mode == 1) {
@@ -102,6 +107,30 @@ void bwd_right(void* mode) {
     bwd(mode);
 }
 
+void R_light(void* unused) {
+    if (right_light == false) {
+        right_light = true;
+        PORTD.OUTSET = PIN6_bm;
+    }
+    
+    else if (right_light == true) {
+        right_light = false;
+        PORTD.OUTCLR = PIN6_bm;
+    }
+}
+
+void L_light(void* unused) {
+    if (left_light == false) {
+        left_light = true;
+        PORTD.OUTSET = PIN7_bm;
+    }
+    
+    else if (left_light == true) {
+        left_light = false;
+        PORTD.OUTCLR = PIN7_bm;
+    }
+}
+
 bool is_signal_combo_active(SignalCombo sig) {
     bool port1_check_1 = sig.port1 ? ((*sig.port1 & sig.mask1) == sig.mask1) : true;
     bool port2_check_1 = sig.port2 ? ((*sig.port2 & sig.mask2) == sig.mask2) : true;
@@ -112,120 +141,8 @@ bool is_signal_combo_active(SignalCombo sig) {
     return (port1_check_1 && port2_check_1 && port1_check_2 && port2_check_2);
 }
 
-
-int main(void) {    
-    CCP = 0xD8;                 // unlock protected I/O registers - page 41
-    CLKCTRL.OSCHFCTRLA = 0x14;  // Clock set to 8 MHz
-    while (CLKCTRL.MCLKSTATUS & 0b00000001) {
-    }
+void signal_check(void) {
     
-    
-    // ******************************************Servo and ultra sonic trigger on TCA0******************************************************************************************* 
-    TCA0.SINGLE.CTRLA = (TCA_SINGLE_CLKSEL_DIV256_gc) | (TCA_SINGLE_ENABLE_bm); // Divide 8MHz by 256 and enable TCA0
-    TCA0.SINGLE.CTRLB = (TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_CMP1EN_bm);  // Enable single slope PWM on CMP0 and CMP1
-    TCA0.SINGLE.PER = 625;                      // Frequency set to 50 Hz page 236: (31250/625) 625, being the PER value
-    
-    // CMP0 will be for trigger and CMP1 will be for the servo
-    
-    TCA0.SINGLE.CMP0 = 1;                          // Can't get to 10us at this TCA0 frequency, shortest is 1/31250 = 32us. should be fine
-    TCA0.SINGLE.CMP1 = 4800 / 100;                 // divide to avoid using decimals the compiler won't keep them
-                                                   // min = 2.75% (17.18), +45° from min = 5.13% (32.03), 0° = 7.5% (46) but 48 is more accurate, +45° from 0 = 10.25% (64.06)
-                                                   // max = 13% (81.25) I will put these in a header file later to make it easier to use
-    
-    PORTA.DIRSET = PIN0_bm;                      // PA0 and PA1 set as output
-    PORTA.DIRSET = PIN1_bm;
-    
-    
-    // **********************************************Motor 1 on TCB0 (PA2) Motor 2 on TCB1 (PA3) and speaker on TCB2 (PC0)*******************************************************
-    
-    TCB0.CTRLB = (TCB_CNTMODE_PWM8_gc) | (TCB_CCMPEN_bm);   // PWM mode and enable WO on PA2 H-
-    TCB1.CTRLB = (TCB_CNTMODE_PWM8_gc) | (TCB_CCMPEN_bm);   // PWM mode and enable WO on PA3 H+
-    TCB2.CTRLB = (TCB_CNTMODE_PWM8_gc) | (TCB_CCMPEN_bm);   // PWM mode and enable WO on PC0
-    
-    // CCMP High byte is the duty and low is the period
-    
-    TCB0.CCMPL = 255;   // 4MHz / 255 = 15686 Hz      // LOW MUST BE SET FIRST
-    TCB0.CCMPH = 0;     // duty
-
-    TCB1.CCMPL = 255;    // 4MHz / 255 = 15686 Hz      
-    TCB1.CCMPH = 0;      // duty
-    
-    TCB2.CCMPL = 50;    // 4MHz / 50 = 10,000 Hz      
-    TCB2.CCMPH = 25;    // duty
-
-    
-    TCB0.CTRLA = (TCB_CLKSEL_DIV2_gc ) | (TCB_ENABLE_bm);     // 4MHz clk and enable TCB
-    TCB1.CTRLA = (TCB_CLKSEL_DIV2_gc ) | (TCB_ENABLE_bm);
-    TCB2.CTRLA = (TCB_CLKSEL_DIV2_gc ) | (TCB_ENABLE_bm);
-    
-    
-    PORTA.DIRSET = PIN2_bm;
-    PORTA.DIRSET = PIN3_bm;
-    PORTC.DIRSET = PIN0_bm;
-    
-    
-    //*********************************************Screen SDA PC2 and SCL PC3 in i2c_avr128db28.c driver*************************************************************************
-    
-    /*
-    i2c_init(); // Initialize I2C
-
-    _delay_ms(500); // Give I2C some time before checking
-    
-    lcd_init();
-    lcd_clrscr();  // **Clear screen before writing**
-    lcd_gotoxy(0, 0);  // **Ensure cursor is at the start**
-    
-    lcd_puts("Hello World1");  // Put string from RAM
-    lcd_gotoxy(0, 2);         // Move cursor to line 3
-    lcd_puts_p(PSTR("String from flash")); 
-    _delay_ms(3000);
-    lcd_clrscr();
-     */
-    
-    //**************************************************************************ADC set up****************************************************************************************
-    
-    // Enable global interrupts.
-    SREG = 0b10000000;
-    
-    // Set the ADC reference level to VDD.
-    VREF.ADC0REF = 0b10000101;
-    
-    // Enable the ADC interrupt.
-    ADC0.INTCTRL = 0b00000001;
-    
-    // Select PD2 (AIN2) as the ADC input.
-    ADC0.MUXPOS = 0x02;
-
-    // Select minimum clock divide.
-    ADC0.CTRLC = 0x00;
-    
-    // Select single ended mode, 12 bit resolution and free-running modes.
-    ADC0.CTRLA = 0b00000011;
-    
-    // Start conversion.
-    ADC0.COMMAND = 0x01;
-    
-    //Ultrasonic set up
-    
-    //Receiver set up
-    
-    PORTA.PIN5CTRL |= PORT_PULLUPEN_bm;
-    PORTA.PIN6CTRL |= PORT_PULLUPEN_bm;
-    
-    PORTD.PIN4CTRL |= PORT_PULLUPEN_bm;
-    PORTD.PIN5CTRL |= PORT_PULLUPEN_bm;
-    
-    PORTA.DIRCLR = PIN5_bm | PIN6_bm;      // PA5 and PA6 as incoming receiver signal D1 and D0 respectivley
-    PORTD.DIRCLR = PIN3_bm | PIN4_bm;      // PD4 and PD5 as incoming receiver signal D3 and D2 respectivley
-    
-    //**********************************************************************LOCALS**************************************************************************************
-    
-    PORTA.DIRSET = PIN7_bm;
-    
-    ActionFunction output = NULL;
-    void* output_arg = NULL;
-    
-    while (1) {
         output = NULL;
         output_arg = NULL;
         
@@ -255,12 +172,13 @@ int main(void) {
         
         else if (is_signal_combo_active(RIGHT_LIGHT)) {
             PORTA.OUTSET = PIN7_bm;
-            output = NULL;
+            output = R_light;
+            output_arg = NULL;
         }
         
         else if (is_signal_combo_active(LEFT_LIGHT)) {
             PORTA.OUTSET = PIN7_bm;
-            output = NULL;
+            output = L_light;
             output_arg = NULL;
         }
         
@@ -304,5 +222,143 @@ int main(void) {
             output = NULL;      
         }
         _delay_ms(10);  // debounce delay
+    
+}
+
+int main(void) {    
+    CCP = 0xD8;                 // unlock protected I/O registers - page 41
+    CLKCTRL.OSCHFCTRLA = 0x14;  // Clock set to 8 MHz
+    while (CLKCTRL.MCLKSTATUS & 0b00000001) {
+    }
+    
+    
+    // ******************************************Servo and ultra sonic trigger on TCA0******************************************************************************************* 
+    TCA0.SINGLE.CTRLA = (TCA_SINGLE_CLKSEL_DIV256_gc) | (TCA_SINGLE_ENABLE_bm); // Divide 8MHz by 256 and enable TCA0
+    TCA0.SINGLE.CTRLB = (TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_CMP1EN_bm);  // Enable single slope PWM on CMP0 and CMP1
+    TCA0.SINGLE.PER = 625;                      // Frequency set to 50 Hz page 236: (31250/625) 625, being the PER value
+    
+    // CMP0 will be for trigger and CMP1 will be for the servo
+    
+    TCA0.SINGLE.CMP0 = 1;                          // Can't get to 10us at this TCA0 frequency, shortest is 1/31250 = 32us. should be fine
+    TCA0.SINGLE.CMP1 = 4800 / 100;                 // divide to avoid using decimals the compiler won't keep them
+                                                   // min = 2.75% (17.18), +45° from min = 5.13% (32.03), 0° = 7.5% (46) but 48 is more accurate, +45° from 0 = 10.25% (64.06)
+                                                   // max = 13% (81.25) I will put these in a header file later to make it easier to use
+    
+    PORTA.DIRSET = PIN0_bm;                      // PA0 and PA1 set as output
+    PORTA.DIRSET = PIN1_bm;
+    
+    
+    // **********************************************Motor 1 on TCB0 (PA2) Motor 2 on TCB1 (PA3) and speaker on TCB2 (PC0)*******************************************************
+    
+    TCB0.CTRLB = (TCB_CNTMODE_PWM8_gc) | (TCB_CCMPEN_bm);   // PWM mode and enable WO on PA2 H-
+    TCB1.CTRLB = (TCB_CNTMODE_PWM8_gc) | (TCB_CCMPEN_bm);   // PWM mode and enable WO on PA3 H+
+    TCB2.CTRLB = (TCB_CNTMODE_PWM8_gc) | (TCB_CCMPEN_bm);   // PWM mode and enable WO on PC0
+    
+    // CCMP High byte is the duty and low is the period
+    
+    TCB0.CCMPL = 255;   // 4MHz / 255 = 15686 Hz      // LOW MUST BE SET FIRST
+    TCB0.CCMPH = 0;     // duty
+
+    TCB1.CCMPL = 255;    // 4MHz / 255 = 15686 Hz      
+    TCB1.CCMPH = 0;      // duty
+    
+    TCB2.CCMPL = 31;    // 31250Hz / 10 = 10,000 Hz      
+    TCB2.CCMPH = 0;    // duty
+
+    
+    TCB0.CTRLA = (TCB_CLKSEL_DIV2_gc ) | (TCB_ENABLE_bm);       // 4MHz clk and enable TCB
+    TCB1.CTRLA = (TCB_CLKSEL_DIV2_gc ) | (TCB_ENABLE_bm);
+    TCB2.CTRLA = (TCB_CLKSEL_TCA0_gc ) | (TCB_ENABLE_bm);       // 31250 Hz from TCA
+    
+    
+    PORTA.DIRSET = PIN2_bm;
+    PORTA.DIRSET = PIN3_bm;
+    PORTC.DIRSET = PIN0_bm;
+    
+    
+    //*********************************************Screen SDA PC2 and SCL PC3 in i2c_avr128db28.c driver*************************************************************************
+    
+    
+    i2c_init(); // Initialize I2C
+
+    _delay_ms(500); // Give I2C some time before checking
+    
+    lcd_init();
+    lcd_clrscr();  // **Clear screen before writing**
+    lcd_gotoxy(0, 0);  // **Ensure cursor is at the start**
+    
+    lcd_puts("Hello World1");  // Put string from RAM
+    lcd_gotoxy(0, 2);         // Move cursor to line 3
+    lcd_puts_p(PSTR("String from flash")); 
+    _delay_ms(3000);
+    lcd_clrscr();
+     
+    
+    //**************************************************************************ADC for metal dector****************************************************************************************
+    
+    // Enable global interrupts.
+    SREG = 0b10000000;
+    
+    // Set the ADC reference level to VDD.
+    VREF.ADC0REF = 0b10000101;
+    
+    // Enable the ADC interrupt.
+    ADC0.INTCTRL = 0b00000001;
+    
+    // Select PD2 (AIN2) as the ADC input.
+    ADC0.MUXPOS = 0x02;
+
+    // Select minimum clock divide.
+    ADC0.CTRLC = 0x00;
+    
+    // Select single ended mode, 12 bit resolution and free-running modes.
+    ADC0.CTRLA = 0b00000011;
+    
+    // Start conversion.
+    ADC0.COMMAND = 0x01;
+    
+    PORTD.DIRCLR = PIN2_bm;
+    
+    //Ultrasonic set up
+    
+    //****************************************************************Receiver set up***********************************************************************************
+    
+    PORTA.PIN5CTRL |= PORT_PULLUPEN_bm;
+    PORTA.PIN6CTRL |= PORT_PULLUPEN_bm;
+    
+    PORTD.PIN4CTRL |= PORT_PULLUPEN_bm;
+    PORTD.PIN5CTRL |= PORT_PULLUPEN_bm;
+    
+    PORTA.DIRCLR = PIN5_bm | PIN6_bm;       // PA5 and PA6 as incoming receiver signal D1 and D0 respectivley
+    PORTD.DIRCLR = PIN3_bm | PIN4_bm;       // PD4 and PD5 as incoming receiver signal D3 and D2 respectivley
+    
+    //Miscellaneous
+    
+    PORTD.DIRSET = PIN6_bm | PIN7_bm;        // Right headlight PD6 and left is PD7
+    
+    //**********************************************************************LOCALS**************************************************************************************
+    
+    PORTA.DIRSET = PIN7_bm;
+    PORTD.DIRSET = PIN5_bm;
+    
+    uint16_t adc_val = 0;
+    while (1) {
+        signal_check();
+        
+        if (ADC0.INTFLAGS & ADC_RESRDY_bm) {
+            ADC0.INTFLAGS = 1;
+            adc_val = ADC0.RES;        
+        }
+        
+        if (adc_val < 1205) {
+                PORTD.OUTSET = PIN5_bm;
+                TCB2.CCMPH = 1;
+                TCB2.CCMPL = (1205 - ADC0.RES) / 38.5;
+        }
+        
+        else {
+            TCB2.CCMPH = 0;
+            PORTD.OUTCLR = PIN5_bm;
+        }
     }
 }
